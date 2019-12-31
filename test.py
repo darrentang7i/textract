b6413c50af9d4e6aea351102895d3726afbe27e3
Watshever <watshever@louieweng@outlook.com>
2019-12-31 15:53:28 -0500

    changed the s3 test file

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import boto3
 
 s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
+response = s3client.list_buckets()
 
 for bucket in s3.buckets.all():
 	print(bucket.name)
+
+print("existing buckets")
+for bucket in response['Buckets']:
+	print(f' {bucket["Name"]}')
